bf3524ee78dd679e41c503d692cf9445fa0c0d77
Albert <albert@lum.ru>
2024-10-25 12:37:05 +0300

    create author controller done

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -6,27 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Author;
 class AuthorController extends AbstractController
 {
-    #[Route('/author_create', name: 'author_create', methods: ['POST','GET'])]
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/author_create', name: 'author_create', methods: ['POST'])]
     public function new(Request $request): Response
     {
 
+        
+        
 
         // Непонятно, почемуто тут $request->request пустой, а данные прилетают в $request->query
         // профайлер бы вклчить для API как нибудь, посмотреть что там происходит
-        $data = $request->request->all();
+        $data = json_decode($request->getContent());
+        $author =  new Author();
+        $author->setName($data->name);
+        $this->entityManager->persist($author);
+        $this->entityManager->flush();
 
-        //$data = json_decode($request->getContent(), true);
-        //$data = $request;
-        //$data = [1,2,3];
         
         
 
         // Обработка данных из запроса (если необходимо)
 
-        $response = new Response(json_encode([$data]), Response::HTTP_OK);
+        $response = new Response(null, Response::HTTP_OK);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
